57718f5700b047a51d1576bcfd00546daa90d5af
Niranjan Singh <niranjan.kala@gmail.com>
2023-04-27 12:48:49 +0530

    Update layout and added images

--- src/FAC.Web/App_Start/BundleConfig.cs
+++ src/FAC.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using log4net.Plugin;
+using System.Runtime;
+using System.Web;
 using System.Web.Optimization;
 
 namespace FAC.Web
@@ -11,6 +13,9 @@ namespace FAC.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -21,10 +26,34 @@ namespace FAC.Web
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+            
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/plugins/revolution/css/settings.css",//< !--REVOLUTION SETTINGS STYLES-- >
+                       "~/plugins/revolution/css/layers.css",//<!-- REVOLUTION LAYERS STYLES -->
+                       "~/plugins/revolution/css/navigation.css",//<!-- REVOLUTION NAVIGATION STYLES -->
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/popper").Include(
+                        "~/Scripts/popper.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/revolution-slider").Include(
+                    "~/plugins/revolution/js/jquery.themepunch.revolution.min.js", 
+                    "~/plugins/revolution/js/jquery.themepunch.tools.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.actions.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.carousel.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.kenburn.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.migration.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.navigation.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.parallax.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.slideanims.min.js" 
+                    ,"~/plugins/revolution/js/extensions/revolution.extension.video.min.js",
+                    "~/Scripts/main-slider-script.js"));
+
+
+
         }
     }
 }
